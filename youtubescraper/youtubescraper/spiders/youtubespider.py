aa6a0a55e486fe2acd22f8759c478a63dc9ad8e8
Janaka dev <janakchamantha12@gmail.com>
2024-08-21 15:36:47 +0530

    feat:get youtube video information

--- youtubescraper/youtubescraper/spiders/youtubespider.py
+++ youtubescraper/youtubescraper/spiders/youtubespider.py
@@ -1,13 +1,29 @@
-from typing import Iterable
+from pytube import YouTube
 import scrapy
 
 
 class YoutubespiderSpider(scrapy.Spider):
     name = "youtubespider"
-    allowed_domains = ["youtube.com"]
+    # allowed_domains = ["youtube.com"]
 
     def start_requests(self):
-        pass
+        url = "https://www.youtube.com/watch?v=wQDklcWKzRA"
+        yield scrapy.Request(url)
 
-    def parse(self, response):
-        pass
+    async def parse(self, response):
+
+        ytres = YouTube(response.url)
+        print(ytres.author)
+        print(ytres.age_restricted)
+        print(ytres.title)
+        # ytres.streaming_data
+        video = ytres.streams
+        # ytres.streams.filter(progressive=True, file_extension="mp4").order_by(
+        #     "resolution"
+        # ).desc().first().download()
+        # print(type(video))
+        # try:
+        #     # video.download("video")
+        # except:
+        #     print("error")
+        # print("**********************************")
